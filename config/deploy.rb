5ad99a0d32c5ca689a96f4ec8e0ceb278d000441
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-06-08 20:11:23 -0400

    setting up capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,33 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :rvm_type                , :user
+set :rvm_ruby_version        , '2.3.1'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :user                    , 'ubuntu'
+set :application             , 'todolist'
+set :deploy_to               , "/home/#{fetch(:user)}/www/#{fetch(:application)}"
+set :scm                     , :git
+set :repo_url                , 'git@github.com:user/reponame.git'
+set :branch                  , :master
+set :keep_releases           , 5
+set :ssh_options             , { forward_agent: true, user: fetch(:user) , keys: %w(~/.ssh/id_rsa.pub) }
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+## Linked Files & Directories (Default None):
+set :linked_files, %w{config/database.yml config/secrets.yml .env}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
-# Default value for :scm is :git
-# set :scm, :git
+set :puma_threads            , [4, 16]
+set :puma_workers            , 1
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
+# Don't change these unless you know what you're doing
+set :pty                     , true
+set :use_sudo                , false
+set :puma_bind               , "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state              , "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid                , "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log         , "#{release_path}/log/puma.access.log"
+set :puma_error_log          , "#{release_path}/log/puma.error.log"
+set :puma_preload_app        , true
+set :puma_worker_timeout     , nil
+set :puma_init_active_record , true  # Change to false when not using ActiveRecord
